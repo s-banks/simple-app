961c115683509e03ca0696a65d2a5266a4f43637
Shane Banks <shane.banks02@gmail.com>
2023-01-04 11:03:51 -0600

    created add admin form and set sorting of table by lastname

--- src/main/java/com/example/appddiction/controllers/HomeController.java
+++ src/main/java/com/example/appddiction/controllers/HomeController.java
@@ -2,23 +2,32 @@ package com.example.appddiction.controllers;
 
 import com.example.appddiction.models.Admin;
 import com.example.appddiction.models.Employee;
+import com.example.appddiction.repositories.AdminRepository;
 import com.example.appddiction.repositories.EmployeeRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Optional;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
 	private final EmployeeRepository employeeDao;
+	private final AdminRepository adminDao;
+	PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-	public HomeController(EmployeeRepository employeeDao) {
+	public HomeController(EmployeeRepository employeeDao, AdminRepository adminDao, PasswordEncoder passwordEncoder) {
 		this.employeeDao = employeeDao;
+		this.adminDao = adminDao;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@GetMapping("/login")
@@ -28,7 +37,9 @@ public class HomeController {
 
 	@GetMapping("/manageusers")
 	public String home(Model model) {
-		model.addAttribute("employees", employeeDao.findAll());
+		List<Employee> employees= employeeDao.findAll();
+		employees.sort(Comparator.comparing(Employee::getLastName));
+		model.addAttribute("employees", employees);
 		model.addAttribute("newEmployee", new Employee());
 		Admin currentAdmin = (Admin) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		model.addAttribute("admin", currentAdmin);
@@ -36,7 +47,7 @@ public class HomeController {
 	}
 
 	@PostMapping("/addusers")
-	public String newUser(Employee employee, HttpServletRequest request, RedirectAttributes rm) {
+	public String newUser(Employee employee) {
 		employeeDao.save(employee);
 		return "redirect:/manageusers";
 	}
@@ -55,6 +66,21 @@ public class HomeController {
 		editEmp.setLastName(request.getParameter("edit-lname"));
 		editEmp.setEmail(request.getParameter("edit-email"));
 		employeeDao.save(editEmp);
+		return "redirect:/manageusers";
+	}
+
+	@PostMapping("/makeadmin")
+	public String makeAdmin(Admin admin) {
+		admin.setPassword(passwordEncoder.encode(admin.getPassword()));
+		char firstLetter = admin.getEmployee().getFirstName().toLowerCase().charAt(0);
+		String uName = firstLetter + admin.getEmployee().getLastName().toLowerCase();
+		if (adminDao.findByUsername(uName) == null) {
+			admin.setUsername(uName);
+			adminDao.save(admin);
+		} else {
+			admin.setUsername(uName + "1");
+			adminDao.save(admin);
+		}
 		return "redirect:/manageusers";
 	}
 
