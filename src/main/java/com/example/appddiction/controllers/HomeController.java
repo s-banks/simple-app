4cfd4903f61dce7b1275927f0a5d880e44a79b2a
s-banks <shroud2@hotmail.com>
2025-03-22 01:11:01 -0500

    added import

--- src/main/java/com/example/appddiction/controllers/HomeController.java
+++ src/main/java/com/example/appddiction/controllers/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
